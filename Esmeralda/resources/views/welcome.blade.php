d96bed8c482a9abb932c055f05cb108fb6d2916d
DaviLimaPereira <davilimapereira@hotmail.com>
2020-10-14 17:38:52 +0000

    v-0.0.1

---
+++ Esmeralda/resources/views/welcome.blade.php
@@ -0,0 +1,68 @@
+
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>E.M. Profª Esmeralda</title>
+
+    <!-- Principal CSS do Bootstrap -->
+    <link href="https://getbootstrap.com.br/docs/4.1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Estilos customizados para esse template -->
+    <link href="https://getbootstrap.com.br/docs/4.1/examples/cover/cover.css" rel="stylesheet">
+
+    <style>
+        body{
+            background-color: #ADD8E6;
+        }
+
+        h1, p{
+            color: black;
+        }
+    </style>
+  </head>
+
+  <body class="text-center">
+
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <header class="masthead mb-auto">
+        <div class="inner">
+          <h3 class="masthead-brand">SIGA</h3>
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link active" href="/">Home</a>
+            <a class="nav-link" href="/home">Conteúdo</a>
+            <a class="nav-link" href="/contato">Contatos</a>
+          </nav>
+        </div>
+      </header>
+
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">Escola Municipal Professora Esmeralda dos Santos Novaes</h1>
+        <p class="lead">Bem vindo à nossa escola!<br>Estamos felizes que você tenha vindo nos visitar.</p>
+        <p class="lead">
+          <a href="/home" class="btn btn-lg btn-secondary">Saiba mais</a>
+        </p>
+      </main>
+
+      <footer class="mastfoot mt-auto">
+        <div class="inner">
+          <p>Criado por <a href="https://github.com/Davi-Lima">Winchester Technologies</a>
+        <br>Desenvolvido pelo Analista de Sistemas <a href="https://br.linkedin.com/in/davi-lima-pereira-5606b6149">Davi Lima Pereira</a>.</p>
+        </div>
+      </footer>
+    </div>
+
+    <!-- Principal JavaScript do Bootstrap
+    ================================================== -->
+    <!-- Foi colocado no final para a página carregar mais rápido -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
