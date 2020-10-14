c5f24a64f10e7ea4c797ca24a121360fd8afd781
DaviLimaPereira <davilimapereira@hotmail.com>
2020-10-14 18:03:36 +0000

    v-0.0.2

--- Esmeralda/resources/views/welcome.blade.php
+++ Esmeralda/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="Prefeitura de Praia Grande">
     <link rel="icon" href="../../../../favicon.ico">
 
     <title>E.M. Profª Esmeralda</title>
@@ -23,6 +23,11 @@
 
         h1, p{
             color: black;
+        }
+
+        a.btn:hover{
+            background-color:chocolate;
+            color:cornsilk;
         }
     </style>
   </head>
@@ -52,7 +57,7 @@
       <footer class="mastfoot mt-auto">
         <div class="inner">
           <p>Criado por <a href="https://github.com/Davi-Lima">Winchester Technologies</a>
-        <br>Desenvolvido pelo Analista de Sistemas <a href="https://br.linkedin.com/in/davi-lima-pereira-5606b6149">Davi Lima Pereira</a>.</p>
+        <br>Desenvolvido pelo Analista de Sistemas <a href="https://br.linkedin.com/in/davi-lima-pereira-5606b6149">Davi Lima Pereira</a></p>
         </div>
       </footer>
     </div>
@@ -61,8 +66,7 @@
     ================================================== -->
     <!-- Foi colocado no final para a página carregar mais rápido -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="../../assets/js/vendor/popper.min.js"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   </body>
 </html>
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # EscolaEsmeralda
 Projeto acadêmico voltado para uma unidade escolar do município.
+
+Versão 0.0.1:
+    Iniciando o projeto com os arquivos base e uma página de boas vindas.
+
+Versão 0.0.2:
+    Criação dos arquivos "Home" e "Contato" para dar seguimento ao prjeto,
+    além das pastas "Layout", "Turmas", "Aluno" e "Prof".
